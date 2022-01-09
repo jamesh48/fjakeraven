4fbffbe0c9c972fa29d433576927befb01d722ad
James Hrivnak <jameshrivnak4@gmail.com>
2022-01-09 10:26:38 -0700

    implemented details service in typescript

---
+++ service-1/server/src/serverTypes.ts
@@ -0,0 +1,13 @@
+export type Item = string;
+export type UrlItem = { url: string };
+export type UrlArr = UrlItem[];
+export type Section = {};
+
+export interface QueryTypes {
+  indicator: string;
+}
+
+export interface ParamTypes {
+  productId: string;
+}
+
